48fd920efd8eb6528a12fb48bcd686a5bfdee39d
Egor4444ik <egor4e4ik@gmail.com>
2023-08-25 18:58:14 +0300

    probe

--- main.py
+++ main.py
@@ -1,6 +1,76 @@
-import numpy
-import tensorflow
-import keras
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras import layers
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
-print("Initial commit")
-#egor
+df_list = []
+df = pd.read_csv(r'C:\Users\gigih\Downloads\train_dataset_Датасет.csv', names=['sentence', 'label'], sep='\t')
+df['source'] = 'mfc'  # Add another column filled with the source name
+df_list.append(df)
+
+df = pd.concat(df_list)
+print(df.iloc[0])
+
+sentences = ['John likes ice cream', 'John hates chocolate.']
+
+vectorizer = CountVectorizer(min_df=0, lowercase=False)
+vectorizer.fit(sentences)
+vectorizer.vocabulary_
+
+vectorizer.transform(sentences).toarray()
+
+df_yelp = df[df['source'] == 'yelp']
+
+sentences = df_yelp['sentence'].values
+y = df_yelp['label'].values
+
+sentences_train, sentences_test, y_train, y_test = train_test_split(
+   sentences, y, test_size=0.25, random_state=1000)
+
+vectorizer = CountVectorizer()
+vectorizer.fit(sentences_train)
+
+X_train = vectorizer.transform(sentences_train)
+X_test  = vectorizer.transform(sentences_test)
+X_train
+
+classifier = LogisticRegression()
+classifier.fit(X_train, y_train)
+score = classifier.score(X_test, y_test)
+
+print("Accuracy:", score)
+
+for source in df['source'].unique():
+    df_source = df[df['source'] == source]
+    sentences = df_source['sentence'].values
+    y = df_source['label'].values
+
+    sentences_train, sentences_test, y_train, y_test = train_test_split(
+        sentences, y, test_size=0.25, random_state=1000)
+
+    vectorizer = CountVectorizer()
+    vectorizer.fit(sentences_train)
+    X_train = vectorizer.transform(sentences_train)
+    X_test  = vectorizer.transform(sentences_test)
+
+    classifier = LogisticRegression()
+    classifier.fit(X_train, y_train)
+    score = classifier.score(X_test, y_test)
+    print('Accuracy for {} data: {:.4f}'.format(source, score))
+
+input_dim = X_train.shape[1]  # Number of features
+
+model = Sequential()
+model.add(layers.Dense(10, input_dim=input_dim, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+model.summary()
+
